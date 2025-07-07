bcb1f06cf10147e3c837192dbe18a8ad83da6eef
jenydster <jenydster@gmail.com>
2025-07-07 13:22:25 +0300

    исправила по РЕР

--- lesson_1_task_5.py
+++ lesson_1_task_5.py
@@ -1,7 +1,7 @@
 def print_num(num):
     print(num, end='')  # end='' убирает перенос строки, чтобы цифры шли подряд
 
-# Вызов функции 11 раз с цифрами номера 88005553535
+
 print_num(8)
 print_num(8)
 print_num(0)
@@ -14,9 +14,9 @@ print_num(5)
 print_num(3)
 print_num(5)
 
-#другое решение
-#def print_num(num):
-    #print(num, end='')
-#number = "88005553535"
-#for digit in number:
- #   print_num(digit)
+# другое решение
+# def print_num(num):
+# print(num, end='')
+# number = "88005553535"
+# for digit in number:
+#   print_num(digit)
